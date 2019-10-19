3bed4cd1cd46bb7164a8f50a33ab9e510b032c95
Steve Miller <smiller@mathworks.com>
2019-10-19 21:12:16 +0200

    Initial commit R2017b

---
+++ Scripts_Data/publish_all_ssc_aircraft_elec.m
@@ -0,0 +1,4 @@
+cd([Elec_Aircraft_Model_HomeDir filesep 'Scripts_Data']);
+sscRepublishDemoScript('Elec_Aircraft_Demo_Script.m');
+cd([Elec_Aircraft_Model_HomeDir filesep 'html']);
+publish_all_html
