cacae95a84d245fa3ba15b9e62f4f1b9a83590ee
Steve Miller <smiller@mathworks.com>
2019-10-19 21:30:43 +0200

    Initial commit R2019b.  Converted to MATLAB Projects.

--- Airtaxi_VTOL/Scripts_Data/publish_all_ssc_airvtol_elec.m
+++ Airtaxi_VTOL/Scripts_Data/publish_all_ssc_airvtol_elec.m
@@ -1,4 +1,25 @@
+% Script to republish all HTML
+
 cd([Elec_Aircraft_VTOL_Model_HomeDir filesep 'Scripts_Data']);
-sscRepublishDemoScript('Elec_Aircraft_VTOL_Demo_Script.m');
+
+filename_info.name = 'Elec_Aircraft_VTOL_Demo_Script.m';
+publish(filename_info.name)
+HTMLfilename = strrep(filename_info.name,'.m','.html');
+movefile(['.' filesep 'html' filesep HTMLfilename],['.' filesep HTMLfilename]);
+rmdir('html');
+
 cd([Elec_Aircraft_VTOL_Model_HomeDir filesep 'html']);
-publish_all_html
+
+function publish_all_html
+filelist_m=dir('*.m');
+
+filenames_m = {filelist_m.name};
+
+warning('off','Simulink:Engine:MdlFileShadowedByFile');
+
+for i=1:length(filenames_m)
+    if ~(strcmp(filenames_m{i},'publish_all_html.m'))
+    publish(filenames_m{i},'showCode',false)
+    end
+end
+end
---
+++ Airtaxi_VTOL/Workflows/Testing/publish_ssc_airvtol_elec.m
@@ -0,0 +1,30 @@
+% Script for testing ssc_airvtol_elec and ssc_airvtol_elec_hybrid
+% and publishing results
+% Copyright 2012-2019 The MathWorks, Inc.
+
+% List of test models 
+% Note that publish scripts do not match model names
+mdl_list = {...
+    'ssc_airvtol_elec',...
+    'ssc_airvtol_elec_hybrid'...
+    };
+
+% Close models to avoid shadowing issues
+for mdl_list_i = 1:length(mdl_list)
+    bdclose(mdl_list{mdl_list_i});
+end
+
+% Move to folder with publish scripts
+cd(fileparts(which(mdl_list{1})))
+cd('Overview')
+
+% Loop over publish scripts
+filelist_m=dir('*.m');
+filenames_m = {filelist_m.name};
+warning('off','Simulink:Engine:MdlFileShadowedByFile');
+for filenames_m_i=1:length(filenames_m)
+    publish(filenames_m{filenames_m_i},'showCode',false)
+end
+
+clear filelist_m filenames_m filenames_m_i
+clear mdl_list mdl_list_i
